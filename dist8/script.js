1b53f59b47214cce44c7c58600bcd242cec54dfd
ritikdutta <ritikduttagd@gmail.com>
2021-01-28 16:29:11 +0530

    dynamic blog-page

--- dist8/script.js
+++ dist8/script.js
@@ -1,1 +1,16 @@
-/* No JavaScript :p */
+function hideToggle(button, elem) {
+ 
+    $(button).toggle(
+      function () {
+        $(elem).hide();
+      },
+      function () {
+        $(elem).show();
+      }
+    );
+        
+    }
+    
+    hideToggle(".button1", ".iframe1");
+    hideToggle(".button2", ".iframe2");
+     
