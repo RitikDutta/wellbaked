8f704e19c7ac5d4dc4b47162bc017ea16b9f9532
ritikdutta <ritikduttagd@gmail.com>
2021-01-30 18:21:24 +0530

    redesigned aboutus

--- dist7/script.js
+++ dist7/script.js
@@ -1,87 +1,132 @@
-$(document).ready(function() {
+(function(){
 
-        // Transition effect for navbar and back-to-top icon
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 550) { 
-              $('.navbar').addClass('solid');
-              $('.back-to-top').addClass('visible'); 
-          } else {
-              $('.navbar').removeClass('solid');
-              $('.back-to-top').removeClass('visible');  
-          }
+  var slideshow = document.getElementById('slideshow'),
+      imgs = slideshow.getElementsByTagName('img'),
+      count = 0,
+      timer,
+      previous;
+  
+  function next(){
+    previous = count > 0 ? count - 1 : 0;
+    imgs[ previous ].className = imgs[ previous ].className.replace(/\bis\-active\b/i, '' );
+    
+    if( count < imgs.length ){
+      imgs[ count ].className += 'is-active';
+      count++;
+    }  
+    else{
+      count = 0;
+      next();
+    }
+  }
+  
+  next();
+  
+  timer = setInterval(function(){
+    next();
+  }, 2000);
+  
+  })();
 
-        });
 
 
-        // Scrolling effect for Arrow icons
-        $("#scrollIcon").click(function(e) {
-            e.preventDefault();
-            $.scrollTo($("#about"), 1000);
-        });
-        $("#nav-about").click(function(e) {
-            e.preventDefault();
-            $.scrollTo($("#about"), 1000);
-        });
-        $("#nav-portfolio").click(function(e) {
-            e.preventDefault();
-            $.scrollTo($("#portfolio"), 1000);
-        });
-        $("#nav-contact").click(function(e) {
-            e.preventDefault();
-            $.scrollTo($("#contact"), 1000);
-        });
-        $(".navbar-brand").click(function(e) {
-            e.preventDefault();
-            $.scrollTo(0, 1000);
-        });
+
+
+
+  (function(){
+
+    var slideshow = document.getElementById('slideshow2'),
+        imgs = slideshow.getElementsByTagName('img'),
+        count = 0,
+        timer,
+        previous;
+    
+    function next(){
+      previous = count > 0 ? count - 1 : 0;
+      imgs[ previous ].className = imgs[ previous ].className.replace(/\bis\-active\b/i, '' );
+      
+      if( count < imgs.length ){
+        imgs[ count ].className += 'is-active';
+        count++;
+      }  
+      else{
+        count = 0;
+        next();
+      }
+    }
+    
+    next();
+    
+    timer = setInterval(function(){
+      next();
+    }, 2000);
+    
+    })();
+  
+  
+  // anim 2
+
+    (function(){
+
+      var slideshow = document.getElementById('slideshow3'),
+          imgs = slideshow.getElementsByTagName('img'),
+          count = 0,
+          timer,
+          previous;
+      
+      function next(){
+        previous = count > 0 ? count - 1 : 0;
+        imgs[ previous ].className = imgs[ previous ].className.replace(/\bis\-active\b/i, '' );
+        
+        if( count < imgs.length ){
+          imgs[ count ].className += 'is-active';
+          count++;
+        }  
+        else{
+          count = 0;
+          next();
+        }
+      }
+      
+      next();
+      
+      timer = setInterval(function(){
+        next();
+      }, 2000);
+      
+      })();
+    
+// anim 3
+
+      
+      (function(){
+
+        var slideshow = document.getElementById('slideshow4'),
+            imgs = slideshow.getElementsByTagName('img'),
+            count = 0,
+            timer,
+            previous;
+        
+        function next(){
+          previous = count > 0 ? count - 1 : 0;
+          imgs[ previous ].className = imgs[ previous ].className.replace(/\bis\-active\b/i, '' );
           
-      });
-
-
-
-      $("#slideshow > div:gt(0)").hide();
-
-      setInterval(function() { 
-        $('#slideshow > div:first')
-          .fadeOut(1000)
-          .next()
-          .fadeIn(1000)
-          .end()
-          .appendTo('#slideshow');
-      },  4000);
-
-
-      $("#slideshow2 > div:gt(0)").hide();
-
-      setInterval(function() { 
-        $('#slideshow2 > div:first')
-          .fadeOut(1000)
-          .next()
-          .fadeIn(1000)
-          .end()
-          .appendTo('#slideshow2');
-      },  4000);
-
-
-      $("#slideshow3> div:gt(0)").hide();
-
-      setInterval(function() { 
-        $('#slideshow3 > div:first')
-          .fadeOut(1000)
-          .next()
-          .fadeIn(1000)
-          .end()
-          .appendTo('#slideshow3');
-      },  4000);
-
-      $("#slideshow4> div:gt(0)").hide();
-
-      setInterval(function() { 
-        $('#slideshow4 > div:first')
-          .fadeOut(1000)
-          .next()
-          .fadeIn(1000)
-          .end()
-          .appendTo('#slideshow4');
-      },  4000);
+          if( count < imgs.length ){
+            imgs[ count ].className += 'is-active';
+            count++;
+          }  
+          else{
+            count = 0;
+            next();
+          }
+        }
+        
+        next();
+        
+        timer = setInterval(function(){
+          next();
+        }, 2000);
+        
+        })();
+      
+      
